69c56d0e53999e07507def5d066664a6b33ee901
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-10 20:38:40 +0530

    Modified - code changes

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,12 +1,14 @@
-import dashboard from './dashboard';
-import pages from './pages';
-import utilities from './utilities';
-import other from './other';
+import dashboard from "./dashboard";
+import pages from "./pages";
+import users from "./Users";
+import other from "./other";
+import orders from "./orders";
+import reports from "./reports";
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, utilities, other]
+  items: [dashboard, reports, orders, users, other],
 };
 
 export default menuItems;
