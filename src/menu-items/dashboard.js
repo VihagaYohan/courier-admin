bf0f629561dd0baace5f3a575dd3cffc86fe6155
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-10 20:17:42 +0530

    Modified - code changes

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -1,21 +1,25 @@
-import { IconDashboard } from "@tabler/icons-react";
+// assets
+import { IconDashboard } from '@tabler/icons-react';
 
+// constant
 const icons = { IconDashboard };
 
+// ==============================|| DASHBOARD MENU ITEMS ||============================== //
+
 const dashboard = {
-  id: "dashboard",
-  title: "Dashboard",
-  type: "group",
+  id: 'dashboard',
+  title: 'Dashboard',
+  type: 'group',
   children: [
     {
-      id: "default",
-      title: "Dashboard",
-      type: "item",
-      url: "/dashboard/default",
+      id: 'default',
+      title: 'Dashboard',
+      type: 'item',
+      url: '/dashboard/default',
       icon: icons.IconDashboard,
-      breadcrumbs: false,
-    },
-  ],
+      breadcrumbs: false
+    }
+  ]
 };
 
 export default dashboard;
