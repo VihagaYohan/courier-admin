bbc51230ec83d3b52be82f9570a52bba641110b9
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-11 14:33:02 +0530

    Added - Side bar items

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./menu.scss";
 
+// data
+import { menu } from "../../data/data";
+
 const Navbar = () => {
-  return <div className="navbar">Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link
+              to={listItem.url}
+              className="listItem"
+              key={`item ${listItem.id}`}
+            >
+              <img src={listItem.icon} alt="" />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+export const menu = [
+  {
+    id: 1,
+    title: "main",
+    listItems: [
+      {
+        id: 1,
+        title: "Home",
+        url: "/",
+        icon: "home.svg",
+      },
+      {
+        id: 2,
+        title: "Profile",
+        url: "/users/1",
+        icon: "user.svg",
+      },
+    ],
+  },
+];
